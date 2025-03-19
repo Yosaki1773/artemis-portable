3864c397339dd416472c57f1af0064870b95b57c
serenade <1920437592@qq.com>
2025-03-20 02:51:57 +0800

    database

--- read.bat
+++ read.bat
@@ -1,5 +1,5 @@
 @echo off
 start "artemis-mariadb" /MIN /D mariadb bin\mysqld.exe --console
-start /B /WAIT /D artemis ..\python\python.exe read.py %*
+start /B /WAIT /D artemis py read.py %*
 
 taskkill /f /fi "WindowTitle eq artemis-mariadb" /t
